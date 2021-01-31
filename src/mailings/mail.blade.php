5f8d5abe430283f3a93d3abf4fa6575721d99b2c
jorgeortegaceja <jorgeortegaceja@gmail.com>
2021-01-30 19:26:37 -0600

    agregue el amiling

---
+++ src/mailings/mail.blade.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible"
+        content="ie=edge">
+    <title>Smart Solutions Comany</title>
+    <style>
+        p,
+        td,
+        tr,
+        body,
+        table {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+
+<body style="background-color: #1ec25a;">
+
+    <table width="650"
+        align="center"
+        cellpadding="0"
+        cellspacing="0"
+        border="0">
+        <tbody>
+            <tr>
+                <td style="text-align: center; color: white; padding: 10px;">
+                    Si no puedes visualizar correctamente este correo
+                    {{-- <a style="color: #ffffff;"
+                        href="https://smartsolutionscompany.com/view-email/{{ $scheduling->html_email_id }}/{{ $user->guid }}">
+                        Da clic aquí
+                    </a> --}}
+                </td>
+            </tr>
+            <tr>
+                <td width="650"
+                    style="background-color: white;">
+                        {{-- <a href="https://smartsolutionscompany.com/resource/link/{{ $scheduling->id }}/{{ $user->guid }}/20efb4ff-9082-477c-b605-552d3eba7d38"
+                        target="_blank">
+                            <img width="220px"
+                                style="display: block; margin: 5px auto;"
+                                src="https://smartsolutionscompany.com/resource/images/{{ $scheduling->id }}/{{ $user->guid }}/6467b17e-cf99-4e7e-9303-3f7126ccbd79"
+                            alt="Smart Solutions Company" />
+                    </a> --}}
+                </td>
+
+            </tr>
+
+            <tr>
+                <td width="650"
+                    style="background-color: white; ">
+                    <img usemap="#workmap"
+                        src="https://smartsolutionscompany.com/images/oficina.jpg"
+                        alt="" />
+
+                    <table width="100%">
+                        <tr>
+                            <td width="65%"
+                                style="padding:0px 15px;">
+                                <h1>Hola @!! Nombre !!</h1>
+                                <p>
+                                    Nos da gusto saludarte. Te enviamos este correo para ofrecerte nuestros servicios
+                                    especializados en desarrollo y marketing. Entendemos la crisis por la que
+                                    atravesamos, es por
+                                    ello que estamos brindando nuestros servicios a ¡bajo costo!.
+                                </p>
+                            </td>
+                            <td width="35%">
+
+                                <img src="https://smartsolutionscompany.com/images/trato.png"
+                                    alt="">
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <br>
+                </td>
+            </tr>
+            <tr style="margin-top: 5px;">
+                <td style="background: white; margin-top: 5px;">
+
+                    <img width="350px"
+                        style="display: block; margin: auto;"
+                        src="https://smartsolutionscompany.com/images/ofertas.png"
+                        alt="">
+                </td>
+            </tr>
+
+
+            <tr>
+                <td style="background: white; ">
+
+                    <img style="display: block; margin: auto; width: 620px; height: auto;"
+                        src="https://smartsolutionscompany.com/images/descuentos.png"
+                        alt=""
+                        srcset="">
+                    <table width="650">
+                        <tr style="height: 340px;">
+                            <td width="50%">
+                                <h2 style="margin: 10px; padding: 15px; color: #ffb155; border: solid 3px transparent;">
+                                    <strong>Descuento de 30%</strong> en tu primera compra
+                                </h2>
+                                <p style="margin: 10px; padding: 15px; color: white; background-color: #ffb155;">
+                                    Canjea este cupón al contratar cualquiera de nuestros servicios
+                                </p>
+                                <p style="margin: 10px; padding: 15px; font-size: 10px">
+                                    * Valido para las primeras 50 personas.
+                                    <br>
+                                    * Cupón no acumulable.
+                                </p>
+                            </td width="50%">
+                            <td
+                                {{-- style="width: 320px; height: 320; vertical-align:middle; background-image: url(https://smartsolutionscompany.com/resource/images/{{ $scheduling->id }}/{{ $user->guid }}/37b0575e-bc51-48c0-b7ad-687bd7fb1c02); background-repeat: no-repeat; text-align: center; background-position: center center; color: #ffffff;"> --}}
+
+                                <h3><strong>Cupón</strong></h3>
+                                <h2>
+                                    SMART09238COV
+                                </h2>
+                            </td>
+                        </tr>
+                    </table>
+
+
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <table width="650"
+                        style="background: #ffffff;">
+                        <tr>
+                            <td style=" background: #ffffff; padding-top :9px; padding-right: 18px; padding-bottom: 15px; padding-left: 18px"
+                                valign="top"
+                                align="center">
+                                <table border="0"
+                                    cellpadding="0"
+                                    cellspacing="0"
+                                    style="border-collapse: separate!important; border: 0px none #000000; border-radius:15px;background-color: #34b5e5">
+                                    <tbody>
+                                        <tr>
+                                            <td align="center"
+                                                valign="middle"
+                                                style="font-family:Arial,Helvetica,sans-serif;font-size:16px;padding:14px">
+                                                {{-- <a title=""
+                                                    href="https://smartsolutionscompany.com/resource/link/{{ $scheduling->id }}/{{ $user->guid }}/e3944401-9688-4afc-a53f-d708045303da"
+                                                    style="letter-spacing:normal;font-style:normal;font-weight:normal;line-height:100%;text-align:center;text-decoration:none;color:#ffffff;display:block;word-break:break-word!important"
+                                                    target="_blank">
+                                                    Hospedaje y Dominio → </a>  --}}
+                                                </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                            <td style=" background: #ffffff; padding-top :9px; padding-right: 18px; padding-bottom: 15px; padding-left: 18px"
+                                valign="top"
+                                align="center">
+                                <table border="0"
+                                    cellpadding="0"
+                                    cellspacing="0"
+                                    style="border-collapse: separate!important; border: 0px none #000000; border-radius:15px;background-color: #34b5e5">
+                                    <tbody>
+                                        <tr>
+                                            <td align="center"
+                                                valign="middle"
+                                                style="font-family:Arial,Helvetica,sans-serif;font-size:16px;padding:14px">
+                                                {{-- <a title=""
+                                                    href="https://smartsolutionscompany.com/resource/link/{{ $scheduling->id }}/{{ $user->guid }}/47da19c6-1179-4c0d-9557-0660a865278f"
+                                                    style="letter-spacing:normal;font-style:normal;font-weight:normal;line-height:100%;text-align:center;text-decoration:none;color:#ffffff;display:block;word-break:break-word!important"
+                                                    target="_blank">
+                                                    Desarrollo web → </a> --}}
+                                                </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+
+                            <td style=" background: #ffffff; padding-top :9px; padding-right: 18px; padding-bottom: 15px; padding-left: 18px"
+                                valign="top"
+                                align="center">
+                                <table border="0"
+                                    cellpadding="0"
+                                    cellspacing="0"
+                                    style="border-collapse: separate!important; border: 0px none #000000; border-radius:15px;background-color: #34b5e5">
+                                    <tbody>
+                                        <tr>
+                                            <td align="center"
+                                                valign="middle"
+                                                style="font-family:Arial,Helvetica,sans-serif;font-size:16px;padding:14px">
+                                                {{-- <a title=""
+                                                    href="https://smartsolutionscompany.com/resource/link/{{ $scheduling->id }}/{{ $user->guid }}/a8247c15-2900-4e01-81d5-386fa0a7819a"
+                                                    style="letter-spacing:normal;font-style:normal;font-weight:normal;line-height:100%;text-align:center;text-decoration:none;color:#ffffff;display:block;word-break:break-word!important"
+                                                    target="_blank">
+                                                    Marketing Digital → </a> --}}
+                                                 </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <br>
+                </td>
+            </tr>
+
+            <tr>
+                <td style="color: #ffffff; text-align: center;">
+                    Si no desea ser parte de nuestra lista de envíos por favor da clic
+                    <a style="color: white;"
+                        href="">AQUÍ</a>.
+                    (Este correo no puede ser considerado spam mientras incluya forma de ser removido). Lee nuestro
+                    aviso de
+                    privacidad
+                    {{-- <a style="color: white;"
+                        href="https://smartsolutionscompany.com/resource/link/{{ $scheduling->id }}/{{ $user->guid }}/de5c0402-4c83-46b6-a198-bb868e0c926d">AQUÍ</a>. --}}
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    <map name="workmap">
+        <area shape="rect"
+            background="#000"
+            coords="385,250,645,280"
+            target="_blank"
+            alt="fanpage"
+            href="https://smartsolutionscompany.com/resource/link/">
+
+    </map>
+</body>
+
+</html>
